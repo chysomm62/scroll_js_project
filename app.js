cabb1d7a94b8bd8abdd7ab2505e9b1b47099a647
chysomm62 <68497765+chysomm62@users.noreply.github.com>
2021-05-11 21:35:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -50,3 +50,49 @@ window.addEventListener('scroll', () => {
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach( (link) => {
+    link.addEventListener('click', (e) => {
+        // prevent default smooth scroll in css
+        e.preventDefault();
+        // get specific spot
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        
+        // calculate the heights
+        const navHeight = navBar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navBar.classList.contains('fixed-nav');
+
+        let position = element.offsetTop - navHeight;
+        if (!fixedNav) {
+            position = position - navHeight;
+        }
+        if (navHeight > 82) {
+            position = position + containerHeight;
+        }
+        window.scrollTo({
+            left:0, top: position,
+        });
+        linksContainer.style.height = 0;
+    });
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
