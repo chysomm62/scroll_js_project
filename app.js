983c987059f3d80b385fc331790e1c3de04ab8b9
chysomm62 <68497765+chysomm62@users.noreply.github.com>
2021-05-11 17:42:59 +0100

    added some elements in html, functionality in js

--- app.js
+++ app.js
@@ -4,10 +4,49 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const linksContainer = document.querySelector('.links-container');
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+const navBar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link')
+
+navToggle.addEventListener('click', () => {
+    // linksContainer.classList.toggle('show-links');
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    
+
+    if (containerHeight === 0){
+        linksContainer.style.height = `${linksHeight}px`;
+    }
+    else {
+        linksContainer.style.height = 0;
+    }
+});
 
 // ********** fixed navbar ************
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navBar.getBoundingClientRect().height;
+
+    if (scrollHeight > navHeight) {
+        navBar.classList.add('fixed-nav');
+    }
+    else {
+        navBar.classList.remove('fixed-nav');
+    }
+
+    if (scrollHeight > 500) {
+        topLink.classList.add('show-link');
+    }
+    else {
+        topLink.classList.remove('show-link')
+    }
+});
 
 // ********** smooth scroll ************
 // select links
